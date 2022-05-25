1b970fbea8b8585140fc71aafd6b3fc7250c72ee
RazraB0tchik <100565729+RazraB0tchik@users.noreply.github.com>
2022-05-26 01:01:59 +1000

    first project on API

---
+++ src/main/java/com/projectapi/apiprojectmk1/ApiProjectMk1Application.java
@@ -0,0 +1,13 @@
+package com.projectapi.apiprojectmk1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjectMk1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiProjectMk1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectapi/apiprojectmk1/ApiProjectMk1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectapi.apiprojectmk1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjectMk1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
